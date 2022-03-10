296fd126b07852a03abb718b7e0b7a070cb23ab9
emyriounis <el.myrioun@gmail.com>
2022-03-10 18:53:41 +0200

    remove unnecesery db call

--- src/auth/registerMiddleware.ts
+++ src/auth/registerMiddleware.ts
@@ -15,58 +15,49 @@ const registerMiddleware = async (
   try {
     const { firstName, lastName, email, password } = req.body;
 
-    const userExists = await ddbClient.send(
-      new GetItemCommand({
+    const newUser = {
+      email: { S: email },
+      password: { S: await encryptPassword(password) },
+      firstName: { S: firstName },
+      lastName: { S: lastName },
+      emailVerified: { BOOL: false },
+    };
+
+    const createdUser = await ddbClient.send(
+      new PutItemCommand({
         TableName: "Users",
-        Key: {
-          email: { S: email },
+        Item: newUser,
+        ConditionExpression: "#email <>  :email",
+        ExpressionAttributeNames: {
+          "#email": "email",
         },
-        AttributesToGet: ["email"],
+        ExpressionAttributeValues: {
+          ":email": { S: email },
+        },
+        ReturnValues: "ALL_OLD",
       })
     );
-    if (userExists.Item) {
-      next(createHttpError(400, "User already exists"));
-    } else {
-      const newUser = {
-        email: { S: email },
-        password: { S: await encryptPassword(password) },
-        firstName: { S: firstName },
-        lastName: { S: lastName },
-        emailVerified: { BOOL: false },
-      };
 
-      const createdUser = await ddbClient.send(
-        new PutItemCommand({
-          TableName: "Users",
-          Item: newUser,
-          ConditionExpression: "#email <>  :email",
-          ExpressionAttributeNames: {
-            "#email": "email",
-          },
-          ExpressionAttributeValues: {
-            ":email": { S: email },
-          },
-          ReturnValues: "ALL_OLD",
-        })
+    if (createdUser) {
+      const token = await generatorJWT(email, "1w");
+      await sendEmail(
+        [email],
+        [],
+        "",
+        `You can validate your email here: ${process.env.FE_URL}/validateEmail/${token}\n\nIt is valid for 1 week`,
+        "Roob. Validate your email"
       );
-
-      if (createdUser) {
-        const token = await generatorJWT(email, "1w");
-        await sendEmail(
-          [email],
-          [],
-          "",
-          `You can validate your email here: ${process.env.FE_URL}/validateEmail/${token}\n\nIt is valid for 1 week`,
-          "Roob. Validate your email"
-        );
-        req.userEmail = email;
-        next();
-      } else {
-        next(createHttpError(400, "Failed to register user"));
-      }
+      req.userEmail = email;
+      next();
+    } else {
+      next(createHttpError(400, "Failed to register user"));
     }
-  } catch (error) {
-    next(error);
+  } catch (error: any) {
+    if (error.name === "ConditionalCheckFailedException") {
+      next(createHttpError(400, "This email is already been used"));
+    } else {
+      next(error);
+    }
   }
 };
 
